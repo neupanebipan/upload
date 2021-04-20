64c7dbe8a59dfd7924ad629c9e2b55c1c564f160
zorrooneeyed <bipanneupane12@gmail.com>
2021-04-20 20:25:45 +0545

    descriptive comment for code changes

---
+++ assignmnet2.py
@@ -0,0 +1,14 @@
+def new_line():
+    print('.')
+def three_lines():
+    new_line()
+    new_line()
+    new_line()
+def nine_lines():
+    three_lines()
+    three_lines()
+    three_lines()
+def clear_screen():
+    new_line()
+    three_lines()
+    nine_lines()
